300060d47ede5827be60bc78735c19a980962afa
rizk <rizkdotai@gmail.com>
2023-10-06 15:18:24 +0300

    Add player Object

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-const Gameboard = (function {
-    let boardArray = [];
+const Gameboard = (function () {
+   // represent Gameboard as a 2D array
+    board: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
 
 })();
+
+const Player = (name, marker) => {
+
+    return {name, marker}
+}
+
+const player1 = Player("Omar", "X")
+const computer = Player("Computer", "O")
+
+console.log(player1.name)
