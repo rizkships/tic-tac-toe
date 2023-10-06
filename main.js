cfe0681324b376d98e641942b607c340ab1768a1
rizk <rizkdotai@gmail.com>
2023-10-06 18:18:29 +0300

    Add render gameBoard func

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Gameboard = (function () {
+const gameBoard = (function () {
    // represent Gameboard as a 2D array
     board: [
         ["", "", ""],
@@ -17,3 +17,32 @@ const player1 = Player("Omar", "X")
 const computer = Player("Computer", "O")
 
 console.log(player1.name)
+
+const displayController = (function() {
+    // render the contents of the gameBoard array to the webpage 
+    const gameBoardContainer = document.getElementById("game-board")
+
+    function renderGameBoard() {
+        // clear any previous content in the container 
+        gameBoardContainer.textContent = ""
+
+        // loop through the gameBoard array 
+        for (let row = 0; row < gameBoard.board.length; row++){
+            for (let col = 0; col < gameBoard.board[row].length; column++) {
+                const cell = document.createElement("div")
+                cell.classList.add("cell")
+                cell.textContent = gameBoard.board[row][column]
+
+                // add event listeners to cells for player moves here
+
+                gameBoardContainer.appendChild("cell")
+
+            }
+        }
+
+        
+    }
+    // expose the renderGameBoard function 
+    return { renderGameBoard }
+
+})()
