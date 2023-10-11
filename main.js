5073316c6753aa8d78c12a2cc844a16584faeb3b
rizk <rizkdotai@gmail.com>
2023-10-11 21:08:59 +0300

    add marker upon clicking

--- main.js
+++ main.js
@@ -3,26 +3,6 @@ const startButton = document.getElementById('start-button')
 startButton.addEventListener("click", ()=>{
     Game.start();;
 })
-const Game = (() => {
-    let players = []
-    let currentPlayerIndex;
-    let gameOver;
-
-const start = () => {
-     players = [
-        createPlayer(document.getElementById('player1').value, "X"),
-        createPlayer(document.getElementById('player2').value, "O")
-
-    ]
-    currentPlayerIndex = 0;
-    gameOver = false;
-    Gameboard.render();
-}
-return {
-    start
-}
-})()
-
 
 
 const createPlayer = (name, marker) => {
@@ -41,11 +21,54 @@ const Gameboard = (() => {
         })
 
         document.getElementById('gameboard').innerHTML = boardHTML
-
+        const squares = document.querySelectorAll(".square")
+        squares.forEach((square) => {
+            square.addEventListener("click", Game.handleClick)
+        })
+        
     }
+
+    const update = (index, value) => {
+        gameboard[index] = value;
+        render();
+    }
+
     return {
       
         render,
+        update
     }
 
 })();
+
+const Game = (() => {
+    let players = []
+    let currentPlayerIndex;
+    let gameOver;
+
+const start = () => {
+     players = [
+        createPlayer(document.getElementById('player1').value, "X"),
+        createPlayer(document.getElementById('player2').value, "O")
+
+    ]
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+    const squares = document.querySelectorAll('.square')
+    squares.forEach((square) => {
+        square.addEventListener("click", handleClick)
+    })
+}
+
+const handleClick = (event) => {
+    let index = parseInt(event.target.id.split("-")[1])
+    Gameboard.update(index, players[currentPlayerIndex].marker)
+
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+}
+return {
+    start,
+    handleClick
+}
+})()
